62bab4541a556a9e7535187d1318bd5ac86bcfbb
gangelo <web.gma@gmail.com>
2013-06-08 21:17:58 -0400

    Save progress

--- test/rspec/engine_spec.rb
+++ test/rspec/engine_spec.rb
@@ -28,5 +28,25 @@ describe 'Engine' do
 
       @engine.action_eval!(action).should == true
     end
+
+    it 'should return action not completed if the action is not completed' do
+
+      action = MonkeyActionPause.new(@monkey, 60)
+      action.action_completed = false
+
+      @monkey.set_action(action)
+
+      @engine.action_eval!(action).should == false
+    end
+
+    it 'should return a new action if a the current action is completed' do
+
+      action = MonkeyActionPause.new(@monkey, 60)
+      action.action_completed = true
+
+      @monkey.set_action(action)
+
+      @engine.new_action(@monkey).should == is_a?(MonkeyActionWake)
+    end
   end
 end
