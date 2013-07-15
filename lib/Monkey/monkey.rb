792db594fbc99c4d28746fe6baddaca5dc995f7e
gangelo <web.gma@gmail.com>
2013-07-14 22:35:23 -0400

    Add more documentation.

--- lib/Monkey/monkey.rb
+++ lib/Monkey/monkey.rb
@@ -7,18 +7,23 @@ require 'ProtectedConstructor'
 # Monkey class.
 #
 # @note The Monkey class represents a virtual monkey that performs particular _actions_
-#   that may or may not be typical of a real monkey.  Monkey _actions_ are derived from class _MonkeyAction_
+#   that may or may not be typical of a real monkey.  Monkey _actions_ are derived from class _MonkeyAction_.
 #
 # @note The constructor for this class is protected; to instantiate a managed _Monkey_, use
-#   MonkeyEngine::MonkeyService#add; to instantiate an unmanaged _Monkey_, use MonkeyEngine::MonkeyManager#add.
+#   MonkeyEngine::MonkeyService#add; to instantiate an unmanaged _Monkey_, use MonkeyFactory::create.
 #
 # @see MonkeyAction
 # @see MonkeyEngine::MonkeyService#add
-# @see MonkeyEngine::MonkeyManager#add
+# @see MonkeyFactory::create
 #
 class Monkey
   include ProtectedConstructor
 
+  # @!attribute [r] monkey_symbol
+  #   @return [Symbol] the _Symbol_ that identifies this Monkey.
+  # @!attribute [r] action
+  #   @return [MonkeyAction] the current _MonkeyAction_ that this _Monkey_ is engaged in.
+  #
   attr_reader :monkey_symbol, :action
 
   protected
@@ -29,20 +34,42 @@ class Monkey
   end
 
   public
+
+  # Determines if the Monkey is alive.
+  #
+  # @note The Monkey is considered alive if the Monkey#thread.alive? is true.
+  #
+  # @return [Boolean] true if the Monkey is alive, false otherwise.
+  #
   def alive?
     return false if @thread.nil?
     @thread.alive?
   end
 
+  # Retrieves the current _Action_ that this Monkey is engaged in.
+  #
+  # @return [MonkeyAction] The _MonkeyAction_.
+  #
   def current_action
     return MonkeyActionDead.new(self) unless alive?
     @action
   end
 
+  # Kills this Monkey.
+  #
+  # @note The Monkey#monkey_do method executed continually by Monkey#thread is
+  #  terminated.
+  #
   def kill
     @kill_thread = true
   end
 
+  # Starts this Monkey.
+  #
+  # @raise [MonkeyEngine::Exceptions::InvalidOperationException] if Monkey#thread is already alive? (started).
+  #
+  # @return [Monkey, self]
+  #
   def start
     raise MonkeyEngine::Exceptions::InvalidOperationException.new "The monkey [#{@monkey_symbol}] thread is already started" \
       if alive?
@@ -50,6 +77,10 @@ class Monkey
     self
   end
 
+  # The thread that this Monkey uses to perform its actions.
+  #
+  # @return [Thread]
+  #
   def thread
     @thread
   end
